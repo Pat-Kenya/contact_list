565fd286ab70801e1dcb370bc80edd8f931ce6ab
pat-kenya <patriciachepkemoi179@gmail.com>
2025-08-06 21:27:02 +0100

    first test

--- contact_list/contact.py
+++ contact_list/contact.py
@@ -1,8 +1,10 @@
 class Contact:
     '''Class contact that generates new instances of contacts '''
+    pass
     contact_list = []
-    def __init__(self, first_name, last_name, email, phone_number):
+    def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
         self.phone_number = phone_number
+        self.email = email
+       
--- contact_list/contact_test.py
+++ contact_list/contact_test.py
@@ -5,3 +5,14 @@ class TestContact(unittest.TestCase):
     Args:
         unittest (TestCase): TestCase class that helps in creating test cases.
     """
+    def setUp(self):
+        """Set up method to run before each test case."""
+        self.new_contact = Contact("James", "muriuki","0712345678", "james@gmail.com")
+    def test_init(self):
+        """Test if the contact object is initialized correctly."""
+        self.assertEqual(self.new_contact.first_name, "James")
+        self.assertEqual(self.new_contact.last_name, "muriuki")
+        self.assertEqual(self.new_contact.phone_number, "0712345678")
+        self.assertEqual(self.new_contact.email, "james@gmail.com")
+if __name__ == '__main__':
+    unittest.main()
