28a75acae4312f6797cdc6d3de6eac7032980b16
pat-kenya <patriciachepkemoi179@gmail.com>
2025-08-06 11:13:47 +0100

    creating contact application

---
+++ week first/contact.py
@@ -0,0 +1,8 @@
+class contact:
+    '''Class contact that generates new instances of contacts '''
+    contact_list = []
+    def __init__(self, first_name, last_name, email, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
